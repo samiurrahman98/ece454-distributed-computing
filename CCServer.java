e4be6831aa10eb7a1626fe3ffa31f3b99877503d
SAMIUR RAHMAN <S72RAHMA@uwaterloo.ca>
2020-05-26 22:59:58 -0400

    receiving data payload

--- CCServer.java
+++ CCServer.java
@@ -3,36 +3,48 @@ import java.net.*;
 
 class CCServer {
     public static void main(String args[]) throws Exception {
-	if (args.length != 1) {
-	    System.out.println("usage: java CCServer port");
-	    System.exit(-1);
-	}
-	int port = Integer.parseInt(args[0]);
-
-	ServerSocket ssock = new ServerSocket(port);
-	System.out.println("listening on port " + port);
-	while(true) {
-	    try {
-		/*
-		  YOUR CODE GOES HERE
-		  - accept connection from server socket
-		  - read requests from connection repeatedly
-		  - for each request, compute an output and send a response
-		  - each message has a 4-byte header followed by a payload
-		  - the header is the length of the payload
-		    (signed, two's complement, big-endian)
-		  - the payload is a string
-		    (UTF-8, big-endian)
-		*/
-		Socket csock = ssock.accept();
-		DataInputStream dataIn = new DataInputStream(csock.getInputStream());
-		while (dataIn.available() > 0) {
-			String k = dataIn.readUTF();
-			System.out.println(k+" ");
+		if (args.length != 1) {
+			System.out.println("usage: java CCServer port");
+			System.exit(-1);
 		}
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
-	}
+
+		int port = Integer.parseInt(args[0]);
+
+		ServerSocket ssock = new ServerSocket(port);
+		System.out.println("listening on port " + port);
+		while(true) {
+			try {
+				/*
+				YOUR CODE GOES HERE
+				- accept connection from server socket
+				- read requests from connection repeatedly
+				- for each request, compute an output and send a response
+				- each message has a 4-byte header followed by a payload
+				- the header is the length of the payload
+					(signed, two's complement, big-endian)
+				- the payload is a string
+					(UTF-8, big-endian)
+				*/
+
+				// block until connection arrives
+				Socket csock = ssock.accept();
+
+				System.out.println("Accepted client: " + csock);
+
+				DataInputStream din = new DataInputStream(csock.getInputStream());
+				int respDataLen = din.readInt();
+				System.out.println("received response header, data payload has length " + respDataLen);
+				byte[] bytes = new byte[respDataLen];
+				din.readFully(bytes);
+				
+				// while (dataIn.available() > 0) {
+				// 	String input = dataIn.readUTF();
+				// 	System.out.println(input + " ");
+				// }
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
     }
 }
