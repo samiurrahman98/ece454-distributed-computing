550a1b6db5a9d381bf0b860ab04d3f92cb7b656e
SAMIUR RAHMAN <S72RAHMA@uwaterloo.ca>
2020-05-28 20:39:46 -0400

    changed implementation to graph, enumeration algorithm complete

--- CCServer.java
+++ CCServer.java
@@ -40,8 +40,8 @@ class CCServer {
 				din.readFully(bytes);
 
 				// Initialize Graph
-				AdjacencyMatrix am = new AdjacencyMatrix();
-				System.out.println("Initialized adjacency matrix.");
+				MGraph mg = new MGraph();
+				System.out.println("Initialized mutable graph.");
 				
 				int i = 0;
 				while (i < bytes.length) {
@@ -64,17 +64,17 @@ class CCServer {
 					}
 					i++;
 
-					am.addEdge(firstNode, secondNode);
+					mg.add(firstNode, secondNode);
 				}
 
-				am.findTriangles();
+				mg.findTriangles();
 
 				System.out.println("Output: ");
-				System.out.println(am.toString());
+				System.out.println(mg.toString());
 
 				// Write graph result to the client
 				DataOutputStream dout = new DataOutputStream(csock.getOutputStream());
-				bytes = am.toString().getBytes("UTF-8");
+				bytes = mg.toString().getBytes("UTF-8");
 				dout.writeInt(bytes.length);
 				dout.write(bytes);
 				dout.flush();
---
+++ MGraph.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import com.google.common.graph.MutableGraph;
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.EndpointPair;
+import com.google.common.base.Joiner;
+
+class MGraph {
+    private MutableGraph<Integer> mGraph = null;
+    private Map<Integer, String> triangleMap = null;
+
+    public MGraph() {
+        mGraph = GraphBuilder.undirected().build();
+    }
+
+    public void add(int firstNode, int secondNode) {
+        mGraph.addNode(firstNode);
+        mGraph.addNode(secondNode);
+        mGraph.putEdge(firstNode, secondNode);
+    }
+
+    public void findTriangles() {
+        triangleMap = new HashMap<Integer, String>();
+        Set<Integer> nodeSet = new HashSet<Integer>();
+
+        int i = 0;
+        Iterator edgeItr = mGraph.edges().iterator();
+        while (edgeItr.hasNext()) {
+            EndpointPair edge = (EndpointPair) edgeItr.next();
+            Iterator nodeItr = mGraph.nodes().iterator();
+            while (nodeItr.hasNext()) {
+                int nodeU = (int) edge.nodeU();
+                int nodeV = (int) edge.nodeV();
+                int nodeW = (int) nodeItr.next();
+                if (nodeV != nodeW && nodeW != nodeU && mGraph.hasEdgeConnecting(nodeV, nodeW) && mGraph.hasEdgeConnecting(nodeW, nodeU)) {
+                    nodeSet.add(nodeU);
+                    nodeSet.add(nodeV);
+                    nodeSet.add(nodeW);
+                    String triangle = Joiner.on(" ").join(nodeSet);
+                    if (!triangleMap.containsValue(triangle)) {
+                        triangleMap.put(i, triangle);
+                        i++;
+                    }
+                    nodeSet.clear();
+                }
+            }
+        }
+    }
+
+    public String toString() {
+        String output = "";
+        int i = 0;
+        int size = triangleMap.values().size();
+        for (String triangle: triangleMap.values()) {
+            output += triangle;
+            if (i != size - 1)
+                output += "\n";
+        }
+        return output;
+    }
+}
