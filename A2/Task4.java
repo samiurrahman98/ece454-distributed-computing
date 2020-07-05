1aa5902d8786eb8aa4f8d09480e0801de69b33c6
SAMIUR RAHMAN <S72RAHMA@uwaterloo.ca>
2020-07-05 13:24:05 -0400

    minor refactor

--- A2/Task4.java
+++ A2/Task4.java
@@ -13,13 +13,13 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+
 import org.apache.hadoop.filecache.DistributedCache;
 import java.net.URI;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.*;  
 
-// Performance: 11.59user 0.92system 0:49.22elapsed 25%CPU (0avgtext+0avgdata 401988maxresident)k
 public class Task4 {
 
   public static class MovieSimilarityMapper extends Mapper<Object, Text, Text, NullWritable> {
@@ -29,19 +29,15 @@ public class Task4 {
     public void setup(Context context) throws IOException, InterruptedException {
       Path cachePath = context.getLocalCacheFiles()[0];
       BufferedReader reader = new BufferedReader(new FileReader(cachePath.toString())); 
-      String line;
       
+      String line;
       while ((line = reader.readLine()) != null){
         String[] tokens = line.split(",", -1);
         String title = tokens[0];
         Byte[] ratings = new Byte[tokens.length - 1];
 
         for (int i = 0; i < ratings.length; i++) {
-          if (!tokens[i + 1].isEmpty()) {
-            ratings[i] = Byte.parseByte(tokens[i + 1]);
-          } else {
-            ratings[i] = 0;
-          }
+          ratings[i] = !(tokens[i + 1].isEmpty()) ? Byte.parseByte(tokens[i + 1]) : 0;
         }
 
         movieRatingsMap.put(title, ratings);
@@ -52,10 +48,10 @@ public class Task4 {
       String title1 = value.toString().split(",", 2)[0];
       Byte[] ratings1 = movieRatingsMap.get(title1);
 
-      for (String title2 : movieRatingsMap.keySet()) {
+      for (String title2: movieRatingsMap.keySet()) {
         if (title2.compareTo(title1) > 0) {
           Byte[] ratings2 = movieRatingsMap.get(title2);
-          int similarity = findSimilarity(ratings1, ratings2);
+          int similarity = getSimilarity(ratings1, ratings2);
 
           result.set(title1 + "," + title2 + "," + similarity);
           context.write(result, NullWritable.get());
@@ -63,7 +59,7 @@ public class Task4 {
       }
     }
 
-    public int findSimilarity(Byte[] ratings1, Byte[] ratings2) {
+    public int getSimilarity(Byte[] ratings1, Byte[] ratings2) {
       int similarity = 0;
 
       for (int i = 0; i < Math.min(ratings1.length, ratings2.length); i++) {
@@ -82,16 +78,15 @@ public class Task4 {
 
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 
-    Job job = new Job(conf, "Task4");
+    Job job = new Job(conf, "Task IV: similarity between movies");
+    
     job.setJarByClass(Task4.class);
-
     job.setMapperClass(MovieSimilarityMapper.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(NullWritable.class);
 
-    // Set up distributed cache
+    // distributed cache
     job.addCacheFile(new URI(otherArgs[0]));
-    // This is a mapper-only hadoop job
     job.setNumReduceTasks(0);
 
     TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
